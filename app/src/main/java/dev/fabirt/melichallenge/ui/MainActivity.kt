42ef217435299c6e22b64e00c9e7fe955fb50e79
Fabián Diartt <fandig27@gmail.com>
2022-04-21 18:51:35 -0500

    first commit

---
+++ app/src/main/java/dev/fabirt/melichallenge/ui/MainActivity.kt
@@ -0,0 +1,43 @@
+package dev.fabirt.melichallenge.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.fabirt.melichallenge.ui.theme.MeliChallengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeliChallengeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Meli Challenge")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MeliChallengeTheme {
+        Greeting("Android")
+    }
+}
